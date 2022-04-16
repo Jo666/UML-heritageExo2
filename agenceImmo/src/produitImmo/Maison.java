34db77686236bf7656ecf04691f38753d1d7f68e
zevil <morlet.geoffrey@gmail.com>
2022-04-16 20:00:23 +0200

    Update Maison.java

--- agenceImmo/src/produitImmo/Maison.java
+++ agenceImmo/src/produitImmo/Maison.java
@@ -3,16 +3,18 @@ package produitImmo;
 public class Maison extends Bien {
 
 	Integer nbEtage;
+	Boolean jardin;
 	
 	public Maison(Integer id, String nom_p, String tel_p, String addr_p, Integer nP, Double montantPrev,
-			 Integer nEtage) {
+			 Integer nEtage, Boolean existe) {
 		super(id, nom_p, tel_p, addr_p, nP, montantPrev);
 		this.nbEtage = nEtage;
+		this.jardin = existe;
 	}
 	
 	@Override
 	public Double getFrais() {
-		return 0.2;
+		return 0.02;
 	}
 
 }
