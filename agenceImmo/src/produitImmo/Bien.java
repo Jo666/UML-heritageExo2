bba94be79a831c9fc0a74f918368b1e84ac76610
zevil <morlet.geoffrey@gmail.com>
2022-04-16 19:59:18 +0200

    Update Bien.java

--- agenceImmo/src/produitImmo/Bien.java
+++ agenceImmo/src/produitImmo/Bien.java
@@ -3,8 +3,7 @@ package produitImmo;
 public abstract class Bien {
 	private Integer id, nPieces;
 	private String nom_prop, tel_prop, add_prop;
-	private Double prixAnnonce, prixVente, frais;
-	private Boolean estVendu;
+	private Double prixAnnonce;
 	public abstract Double getFrais();
 
 	public Bien(Integer id, String nom_p, String tel_p, String addr_p, Integer nP, Double montantPrev) {
